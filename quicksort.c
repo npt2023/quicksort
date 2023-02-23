1cd2299c137a82eba41a7515d8bf844a2300337e
npt2023 <68345847+npt2023@users.noreply.github.com>
2023-02-23 00:31:17 -0500

    Fix indentation

--- quicksort.c
+++ quicksort.c
@@ -29,33 +29,33 @@
 
 
 void quicksort(int *arr, int low, int high) {
-	if(low >= high) return;
-	int i = low;
-	int j = high-1;
-	for(;;)
-	{
-		while(arr[i] <= arr[high] && i < high) {
-			i++;	
-		}
-		while(arr[j] > arr[high] && j > low) {
-			j--;
-		}
-		/* Prints the array with respect to i, j, and the partitioning
-		 * element
-		 */
-		debug_arr(arr, i, j, high, ARRAY_LENGTH);
-		if(i < j)
-		{
-			swap(&arr[i], &arr[j]);
-		}
-		if(j <= i)
-		{
-			swap(&arr[i], &arr[high]);
-			break;
-		}
+    if(low >= high) return;
+    int i = low;
+    int j = high-1;
+    for(;;)
+    {
+        while(arr[i] <= arr[high] && i < high) {
+            i++;	
+        }
+	while(arr[j] > arr[high] && j > low) {
+	    j--;
 	}
-	quicksort(arr, low, i-1);
-	quicksort(arr, i+1, high);
+	/* Prints the array with respect to i, j, and the partitioning
+	 * element
+	 */
+	debug_arr(arr, i, j, high, ARRAY_LENGTH);
+	if(i < j)
+	{
+   	    swap(&arr[i], &arr[j]);
+	}
+	if(j <= i)
+	{
+	    swap(&arr[i], &arr[high]);
+	    break;
+	}
+    }
+    quicksort(arr, low, i-1);
+    quicksort(arr, i+1, high);
 }
 
 /*
@@ -66,7 +66,7 @@ void quicksort(int *arr, int low, int high) {
  *
  */
 void delay(unsigned int milliseconds) {
-	usleep((useconds_t) milliseconds * 1000);
+    usleep((useconds_t) milliseconds * 1000);
 }
 
 
@@ -89,59 +89,58 @@ void delay(unsigned int milliseconds) {
 
 void debug_arr(int *arr, int i, int j, int pivot, int array_length)
 {
-	int pos;
+    int pos;
     bool isInitialized = false;
-	HelperObject myObject = create();
-	printf("Value of i: %d, value of j: %d\n", i, j);
-	printf("Value of pivot p: %d\n", pivot);
-	/* 
-	 * Traverse the array. Print a newline for every 10 elements.
-	 * When a pivot element, i, or j detected, call the helper function
-	 * to print their relative location under the array segment
-	 *
-	 */
-	for(pos = 0; pos <= array_length; pos++) {
-		if(Dec(pos) == 0 || pos == array_length) { // Print a newline every 10 elements, or when array
-		//_length is reached
-			printf("\n");
+    HelperObject myObject = create();
+    printf("Value of i: %d, value of j: %d\n", i, j);
+    printf("Value of pivot p: %d\n", pivot);
+    /* 
+     * Traverse the array. Print a newline for every 10 elements.
+     * When a pivot element, i, or j detected, call the helper function
+     * to print their relative location under the array segment
+     *
+     */
+    for(pos = 0; pos <= array_length; pos++) {
+        if(Dec(pos) == 0 || pos == array_length) { // Print a newline every 10 elements, or when array
+        //_length is reached
+            printf("\n");
             if(isInitialized) {
                 run(myObject);
                 uninitialize(myObject);
                 isInitialized = false;      
                 printf("\n");
-			}
-		}
+	    }
+        }
         if(pos == array_length) break;
-		printf("%4d", arr[pos]); // Print each element one by one
-		if(pos == i) {
+        printf("%4d", arr[pos]); // Print each element one by one
+        if(pos == i) {
             isInitialized = true;
-			initialize(myObject, "i", Dec(i));
-		}
-		if(pos == j) {
+            initialize(myObject, "i", Dec(i));
+        }
+        if(pos == j) {
             isInitialized = true;
-			initialize(myObject, "j", Dec(j));
-		}
-		if(pos == pivot) {
+	    initialize(myObject, "j", Dec(j));
+        }
+        if(pos == pivot) {
             isInitialized = true;
-			initialize(myObject, "p", Dec(pivot));
-		}
-	}
-	/* We are done */	
-
-	print_dashline(50);
+	    initialize(myObject, "p", Dec(pivot));
+        }
+    }
+/* We are done */
+    print_dashline(50);
 }
 
 	
 
 void swap(int *a, int *b) {
-	int tmp = *a;
-	*a = *b, *b = tmp;
-	return;
+    int tmp = *a;
+    *a = *b, *b = tmp;
+    return;
 }
 
 void print_dashline(int n) {
-	int i = 0;
-	while(i++ < n)
-		putchar('-');
-	putchar('\n');
+    int i = 0;
+    while(i++ < n)
+        putchar('-');
+    putchar('\n');
 }
